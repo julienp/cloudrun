7eb9064faa34e475c1d487ca03e2dd027afe2f33
Julien Poissonnier <julien@caffeine.lu>
2025-02-10 11:11:53 +0100

    Fix schema
    pulumi 3.149.0 tries to infer the name based on the path if metadata is not provided, however it does so in a naive way that results in an invalid name.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
-from pulumi.provider.experimental import component_provider_host
+from pulumi.provider.experimental import Metadata, component_provider_host
 
 if __name__ == "__main__":
     # Call the component provider host. This will discover any ComponentResource
     # subclasses in this package, infer their schema and host a provider that
     # allows constructing these components from a Pulumi program.
-    component_provider_host()
+    component_provider_host(Metadata("cloudrun", "0.0.1"))
