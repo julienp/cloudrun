408f6cc1c123921cea8023bb1920be5bda6c36e9
Julien Poissonnier <julien@caffeine.lu>
2025-02-10 11:03:24 +0100

    typo fix

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from pulumi.provider.experimental import component_provider_host
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     # Call the component provider host. This will discover any ComponentResource
     # subclasses in this package, infer their schema and host a provider that
     # allows constructing these components from a Pulumi program.
