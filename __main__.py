3075ee8e5e5a369b02fad62f8e2c70286799ff7d
Julien Poissonnier <julien@caffeine.lu>
2025-02-10 18:02:38 +0100

    bump to 2.1.0

--- __main__.py
+++ __main__.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     # Call the component provider host. This will discover any ComponentResource
     # subclasses in this package, infer their schema and host a provider that
     # allows constructing these components from a Pulumi program.
-    component_provider_host(Metadata("cloudrun", "0.0.1"))
+    component_provider_host(Metadata("cloudrun", "2.1.0"))
